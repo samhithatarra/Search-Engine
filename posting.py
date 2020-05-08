85f77f3d17269478f0e5482cbfe575602260e991
vaanyag <vaanyag@uci.edu>
2020-05-09 03:03:27 +0530

    made merge and diff tokeniser

--- posting.py
+++
@@ -1,7 +0,0 @@
-class Posting:
-    def __init__(self, docId, header_freq, body_freq, token_frequency):
-        self.docId = docId
-        self.header_freq = header_freq
-        self.body_freq = body_freq
-        #self.tf_idf_score = 0.0
-        self.token_freq = token_frequency
