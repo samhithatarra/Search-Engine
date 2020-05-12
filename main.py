11d8d5e3c907a181435e910bb2a51074de94f46a
Kamaniya Sathish <kamaniya.sathishkumar@hotmail.com>
2020-05-11 17:12:27 -0700

    completed code for milestone 2 (top 5 urls and search time for queries)

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ from collections import OrderedDict
 from merge import merge_files
 import os
 import lxml 
+import ast
 indexer_list = dict()
 docId = 0
 num_unique_tokens = 0
@@ -133,89 +134,95 @@ def frequency_calculation(unique_tokens,tags_dict):
 
 if __name__ == "__main__":
 
-    counter_file = 0
-    path_direc = "/Users/kamaniya/Documents/Search-Engine/DEV.nosync"  
-    for direc in pathlib.Path(path_direc).iterdir():
+    # counter_file = 0
+    # path_direc = "/Users/kamaniya/Documents/Search-Engine/DEV.nosync"  
+    # for direc in pathlib.Path(path_direc).iterdir():
         
-        for path in pathlib.Path(direc).iterdir():
-            if path.is_file():
-                docId+=1
-                total_token_count = 0
-                unique_tokens = []
-                #if docId > 4200:
-                tags_dict = find_content(path, docId)
+    #     for path in pathlib.Path(direc).iterdir():
+    #         if path.is_file():
+    #             docId+=1
+    #             total_token_count = 0
+    #             unique_tokens = []
+            
+    #             tags_dict = find_content(path, docId)
 
-                for tag, words_lst in tags_dict.items():
+    #             for tag, words_lst in tags_dict.items():
                 
-                    (token_freq_dict,unique_tokens,token_lst) = find_tokens(words_lst,unique_tokens)
-                #ps = PorterStemmer()
-                #token_freq_dict = defaultdict(int)
-                    if token_lst!=None:
-                        #for token in token_lst:
-                            #token=ps.stem(token)
-                            #if token not in unique_tokens:
-                                #unique_tokens.append(token)
-                            #count_tokens.append(token)
-                            #token_freq_dict[token]+=1
-                            #total_token_count +=1
-                        tags_dict[tag] = token_freq_dict
-                        num_unique_tokens += len(unique_tokens)
+    #                 (token_freq_dict,unique_tokens,token_lst) = find_tokens(words_lst,unique_tokens)
+      
+    #                 if token_lst!=None:
+    #                     tags_dict[tag] = token_freq_dict
+    #                     num_unique_tokens += len(unique_tokens)
                     
-                    frequency_calculation(unique_tokens,tags_dict)
-                print(docId)
-            # Offloading 4 times 
-            file_lst = [14000,28000,42000,55393]
-            PathToFile = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format(counter_file)
-            dict_ = {}
-            if docId in file_lst:
-                with open(PathToFile,'w+') as f:
-                    for tokens, postings in sorted(indexer_list.items(), key=lambda item: item[0]):
-                        dict_[tokens] = dict(postings)
-                        f.write(str(dict_))
-                        f.write('\n')
-                        dict_={}
-                indexer_list.clear()
-                counter_file+=1
+    #                 frequency_calculation(unique_tokens,tags_dict)
+    #             print(docId)
+    #         # Offloading 4 times 
+    #         file_lst = [14000,28000,42000,55393]
+    #         PathToFile = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format(counter_file)
+    #         dict_ = {}
+    #         if docId in file_lst:
+    #             with open(PathToFile,'w+') as f:
+    #                 for tokens, postings in sorted(indexer_list.items(), key=lambda item: item[0]):
+    #                     dict_[tokens] = dict(postings)
+    #                     f.write(str(dict_))
+    #                     f.write('\n')
+    #                     dict_={}
+    #             indexer_list.clear()
+    #             counter_file+=1
                     
 
-    # ********** MERGING ************************** 
-    file1 = "/Users/kamaniya/Documents/Search-Engine/0.txt"
-    file2 = "/Users/kamaniya/Documents/Search-Engine/1.txt"
-    file3 = "/Users/kamaniya/Documents/Search-Engine/2.txt"
-    file4 = "/Users/kamaniya/Documents/Search-Engine/3.txt"
-    merge_1 = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format('merge_1')
-    merge_2 = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format('merge_2')
-    final_merge = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format('final_merge')
-    merge_files(file1,file2,merge_1)
-    merge_files(file3,file4,merge_2)
-    merge_files(merge_1,merge_2,final_merge)
+    # # ********** MERGING ************************** 
+    # file1 = "/Users/kamaniya/Documents/Search-Engine/0.txt"
+    # file2 = "/Users/kamaniya/Documents/Search-Engine/1.txt"
+    # file3 = "/Users/kamaniya/Documents/Search-Engine/2.txt"
+    # file4 = "/Users/kamaniya/Documents/Search-Engine/3.txt"
+    # merge_1 = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format('merge_1')
+    # merge_2 = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format('merge_2')
+    # final_merge = "/Users/kamaniya/Documents/Search-Engine/{}.txt".format('final_merge')
+    # merge_files(file1,file2,merge_1)
+    # merge_files(file3,file4,merge_2)
+    # merge_files(merge_1,merge_2,final_merge)
 
-    # ********** INDEX OF INDEX *******************
-    doc = open("/Users/kamaniya/Documents/Search-Engine/urls.txt",'w')
-    doc.write(str(doc_url))
-    index_of_index = {}
-    index_of_index['digit'] = 1
-    f = open("/Users/kamaniya/Documents/Search-Engine/final_merge.txt",'r') 
-    alphabet = 'a'
-    line_number  = 0
-    ord_a = ord(alphabet)   
-    for line in f:
-        line_number +=1
-        if line.startswith("{'"+alphabet):
-            alphabet = chr(ord(alphabet)+1)
-            index_of_index[chr(ord(alphabet)-1)] = line_number
-        elif alphabet == 'z' and line.startswith("{'"+alphabet):
-            index_of_index[alphabet] = line_number
-            break
+    # # ********** INDEX OF INDEX *******************
+    # doc = open("/Users/kamaniya/Documents/Search-Engine/urls.txt",'w')
+    # doc.write(str(doc_url))
+    # doc.close()
+    # index_of_index = {}
+    # index_of_index['digit'] = 1
+    # f = open("/Users/kamaniya/Documents/Search-Engine/final_merge.txt",'r') 
+    # alphabet = 'a'
+    # line_number  = 0
+    # ord_a = ord(alphabet)   
+    # for line in f:
+    #     line_number +=1
+    #     if line.startswith("{'"+alphabet):
+    #         alphabet = chr(ord(alphabet)+1)
+    #         index_of_index[chr(ord(alphabet)-1)] = line_number
+    #     elif alphabet == 'z' and line.startswith("{'"+alphabet):
+    #         index_of_index[alphabet] = line_number
+    #         break
       
     
-    index_index = open("/Users/kamaniya/Documents/Search-Engine/index_of_index.txt",'w')
+    # f.close()
+    # ************ INDEX OF INDEX (each token) ******************
+    index_of_index = {}
+    counter = 0
+    f = open("/Users/kamaniya/Documents/Search-Engine/final_merge.txt",'r') 
+    for line in f:
+        counter+=1
+        line = ast.literal_eval(line)
+        line_key = [*line.keys()][0]
+        index_of_index[line_key] = counter
+    f.close()
+    index_index = open("/Users/kamaniya/Documents/Search-Engine/index_of_index-1.txt",'w')
     index_index.write(str(index_of_index))
+    index_index.close() 
+    
 
-    print("************** FEEDBACK REPORT **************")
-    print("Number of Indexed Documents:", docId)
-    print("Number of Unique Words:", len(set(count_tokens)))
-    complete_index_file = "/Users/kamaniya/Documents/Search-Engine/final_merge.txt"
-    file_info = os.stat(complete_index_file)
-    print("Complete Index File Size in KB: ", float(file_info.st_size) / 1000)
-    print("************** FEEDBACK REPORT **************")
+    # print("************** FEEDBACK REPORT **************")
+    # print("Number of Indexed Documents:", docId)
+    # print("Number of Unique Words:", len(set(count_tokens)))
+    # complete_index_file = "/Users/kamaniya/Documents/Search-Engine/final_merge.txt"
+    # file_info = os.stat(complete_index_file)
+    # print("Complete Index File Size in KB: ", float(file_info.st_size) / 1000)
+    # print("************** FEEDBACK REPORT **************")
--- query.py
+++ query.py
@@ -2,13 +2,30 @@
 
 from nltk.stem import PorterStemmer
 import re
+import ast 
+import timeit
 
+def get_seek():
 
-'''def get_query():
+    f = open("/Users/kamaniya/Documents/Search-Engine/final_merge.txt", 'r')
+    line_offset = []
+    offset = 0
+    for line in f:
+        line_offset.append(offset)
+        offset += len(line)
+    f.close()
+    return(line_offset)
+
+def get_query():
+    
     query = input('Enter the query to be searched: ')
-    return query
+    global start_time 
+    start_time =  timeit.default_timer()
+    query_lst = tokenize_query(query)
+    return query_lst
 
 def tokenize_query(query):
+
     ps = PorterStemmer()
     token_lst = []
     q_text = query.lower()
@@ -19,62 +36,85 @@ def tokenize_query(query):
             token=ps.stem(token)
             token_lst.append(token)
     return token_lst
-'''
-def find_query():
+
+def extract_posting(query_list):
     
-    f = open("/Users/sarthakgupta/Search-Engine/0-.txt", 'r')
-    #f.seek(100)
-    #print (f.readline())
-    #[0,100, 145,...]
-    line_offset = []
-    offset = 0
-    for line in f:
-        line_offset.append(offset)
-        offset += len(line)
-    f.seek(0)
-    #counter = dict[a]
-    #while counter!=line_offset(dict[b])
-    token = 'unkdkdkf'
-    print (index_of_index['a'])
-    counter = 0
-    if token[0] !='z':
-        order_ = ord(token[0])+1
-        f.seek(line_offset[index_of_index[chr(order_)]-1])
-        last_line = f.readline()  # line that starts from v 
-    else: 
-        last_line = ''
-    f.seek(line_offset[index_of_index[token[0]]-1]) #16223
-    print (index_of_index['u'])
-    line = f.readline() #16223
-    line_found = 'place holder'
-    while counter !=1:
-        if token in line:
-            counter =1
-            line_found = line
-            break
-        if line == last_line:
-            #print (last_line)
-            break
-        line = f.readline()
-    print (line_found)
+    posting_list = []
+    posting_dict = {}
+    f = open("/Users/kamaniya/Documents/Search-Engine/final_merge.txt", 'r')
+    line_offset = get_seek()
+    #counter = 0
+    for token in query_list:
+        counter = 0
+        f.seek(line_offset[index_of_index[token]-1])  # gives the line from where we have to start searching
+        line = f.readline()  # reads the first line from where we have to start  after seek
+        #print (line)
+        '''if token[0] == 'z':
+            last_line = ''
+        elif token[0].isdigit():
+            last_line = line_offset[index_of_index['a']]
+        else:
+            order_ = ord(token[0])+1
+            f.seek(line_offset[index_of_index[chr(order_)]-1]) # this finds the last line -- where the loop should end. 
+            last_line = f.readline()  # reads the last line
+        f.seek(line_offset[index_of_index[token[0]]-1])  # gives the line from where we have to start searching
+        line = f.readline()  # reads the first line from where we have to start  after seek
+        line_found = 'place holder'  # random string - if we don't find the token
+        while counter !=1:
+            line = ast.literal_eval(line)
+            token_found = [*line.keys()][0]
+            if token == token_found:
+                counter =1
+                line_found = token_found
+                break
+            elif line == last_line:
+                break
+            line = f.readline()'''
+        #if counter == 1:
+            #line_found = line_found.strip()
+        line = ast.literal_eval(line)
+        dict_ = line
+        key = [*dict_.keys()][0]
+        value = dict_[key]
+        #print(value)
+        posting_dict[key] = value
+        posting_list.append(posting_dict)
+
+    #print (posting_dict)
+    f.close()
+    #return 
+    find_query(posting_dict)
+    #return posting_dict
     
-index_of_index = {}
-f = open("/Users/sarthakgupta/Search-Engine/0-.txt",'r')
-#counter = 0
-alphabet = 'a'
-line_number  = 0
-ord_a = ord(alphabet)   #65x
-index_of_index['numbers'] = 1
-for line in f:
-    line_number +=1
-    if line.startswith("{'"+alphabet) :
-        index_of_index[alphabet] = line_number
-        alphabet = chr(ord(alphabet)+1)
-        index_of_index[alphabet] = line_number
-    if alphabet =='z':
-        break
-print (index_of_index)
-find_query()
-'''query = get_query()
-token_lst = tokenize_query(query)
-find_query(token_lst)'''
+def find_query(posting_dict):
+    global start_time
+    #stop = timeit.default_timer()
+    list_docId =[]
+    for token, posting in posting_dict.items():
+        list_docId.append(set(posting.keys()))
+    
+    set_intersection  = set.intersection(*list_docId)
+    stop = timeit.default_timer()
+    print ("TIME:", (stop - start_time) * 1000, 'milliseconds')
+    print()
+    set_intersection = list(set_intersection)
+    for i in range(0, 5):
+        print(urls[set_intersection[i]])
+
+if __name__ == "__main__":
+    f2 = open("/Users/kamaniya/Documents/Search-Engine/urls.txt", 'r')
+    line = f2.read()
+    urls = ast.literal_eval(line)
+    f2.close()
+    start_time = timeit.default_timer()
+    index_of_index = {}
+    f = open("/Users/kamaniya/Documents/Search-Engine/index_of_index-1.txt", 'r')
+    index = f.read()
+    index_of_index = ast.literal_eval(index)
+    query = get_query()
+    extract_posting(query)
+    f.close()
+    print() 
+ 
+
+  
